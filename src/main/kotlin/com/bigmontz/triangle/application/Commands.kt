c6910c455a7a997cd5a012f3404998c5f9655ea0
Antonio Araujo <anthenriqufrj@gmail.com>
2018-07-24 23:22:32 -0300

    Some javadoc

--- src/main/kotlin/com/bigmontz/triangle/application/Commands.kt
+++ src/main/kotlin/com/bigmontz/triangle/application/Commands.kt
@@ -7,18 +7,33 @@ import org.springframework.shell.standard.ShellMethod
 import java.math.BigDecimal
 import java.math.RoundingMode
 
+/**
+ * Class which handles shell commands
+ */
 @ShellComponent
 class Commands (
         @Value("\${bigdecimal.scale}") val scale : Int,
         @Value("\${bigdecimal.roundingMode}") val roundingMode: RoundingMode
     ) {
 
+    /**
+     * Command which calculates the triangle type
+     * @param a The a side
+     * @param b The b side
+     * @param c The c side
+     */
     @ShellMethod("Tells the triangle type")
-    fun triangle(a: BigDecimal, b : BigDecimal, c : BigDecimal) {
+    fun triangleType(a: BigDecimal, b : BigDecimal, c : BigDecimal) {
         val triangle = Triangle(scaleIt(a), scaleIt(b), scaleIt(c))
         println("The Triangle ($a, $b, $c) is ${triangle.type}")
     }
 
+    /**
+     * Receives an BigDecimal and Scale It
+     *
+     * @param value The value which will be scaled
+     * @return The value scaled
+     */
     private fun scaleIt(value : BigDecimal) : BigDecimal {
         return value.setScale(scale, roundingMode)
     }
